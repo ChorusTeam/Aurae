de235b937836074a6ac05fae207635b01ec77775
YeoXuHang <90314008+YeoXuHang@users.noreply.github.com>
2024-10-08 21:27:31 +0800

    Update Ambiance.java

--- src/main/java/net/yeoxuhang/ambiance/Ambiance.java
+++ src/main/java/net/yeoxuhang/ambiance/Ambiance.java
@@ -3,7 +3,6 @@ package net.yeoxuhang.ambiance;
 import eu.midnightdust.lib.config.MidnightConfig;
 import net.fabricmc.api.ModInitializer;
 import net.yeoxuhang.ambiance.config.AmbianceConfig;
-import net.yeoxuhang.ambiance.config.TestConfig;
 import net.yeoxuhang.ambiance.core.Registries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -17,7 +16,6 @@ public class Ambiance implements ModInitializer {
     public void onInitialize() {
         LOGGER.info("A small Mod that add new ambient effects to the Minecraft!");
         Registries.init();
-        //MidnightConfig.init(MOD_ID, TestConfig.class);
         MidnightConfig.init(MOD_ID, AmbianceConfig.class);
     }
 }
