88ea9e23aaeebd78b21ed264fcfc4f5c2a622660
YeoXuHang <90314008+YeoXuHang@users.noreply.github.com>
2024-12-06 11:03:26 +0800

    End remastered crash fixed

--- common/src/main/java/net/yeoxuhang/ambiance/util/NbtGetter.java
+++ common/src/main/java/net/yeoxuhang/ambiance/util/NbtGetter.java
@@ -1,10 +1,7 @@
 package net.yeoxuhang.ambiance.util;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.NbtComponent;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.Property;
 
@@ -12,7 +9,6 @@ public class NbtGetter {
     public static String getBlockStateProperty(Level world, BlockPos pos, String propertyName) {
         // Get the BlockState at the specified position
         BlockState blockState = world.getBlockState(pos);
-
         // Check if the BlockState is valid
         if (blockState != null) {
             // Retrieve the property by name from the BlockState's block
@@ -21,7 +17,7 @@ public class NbtGetter {
             if (property != null) {
                 // Get the value of the property and convert it to a string
                 Comparable<?> value = blockState.getValue(property);
-                String valueStr = value.toString();
+                String valueStr = value.toString().toLowerCase().replace("_eye", "");
                 return valueStr;
             } else {
                 System.out.println("Property '" + propertyName + "' does not exist for this block.");
